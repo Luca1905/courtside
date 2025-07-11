729bf0f6c383de0194cac792d73b5926f37b6955
Luca1905 <contact@lucawang.me>
2025-07-11 16:31:22 +0200

    fixed tab bar active tint color

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,21 @@
 import { Tabs } from 'expo-router';
 import { ClockFading, Play } from 'lucide-react-native';
 import React from 'react';
-import { Platform } from 'react-native';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: '#888888',
         headerShown: false,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
+        tabBarStyle: {
+          position: "absolute" 
+        },
+        tabBarActiveTintColor: '#ff0000',
+        tabBarInactiveTintColor: '#888888',
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: '600',
+        }
       }}>
       <Tabs.Screen
         name="index"
