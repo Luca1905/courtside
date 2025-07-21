2d16298f49982ef85cb0fd689fd779ee5a3de2a1
Luca Wang <contact@lucawang.me>
2025-07-21 22:47:25 +0200

    FEAT: mock refreshing for match history

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, View, FlatList } from 'react-native';
+import { SafeAreaView, View, FlatList, RefreshControl } from 'react-native';
 import { Text } from "~/components/ui/text";
 
 import { MatchCard } from '~/components/MatchCard';
@@ -6,14 +6,21 @@ import { useQuery } from 'convex/react';
 import { api } from '~/convex/_generated/api';
 
 import HomeScreenSkeleton from '~/components/HomeScreenSkeleton';
+import { useState } from 'react';
 
 export default function HomeScreen() {
   const matches = useQuery(api.matches.get);
+  const [refreshing, setRefreshing] = useState(false);
 
   // Loading state
   if (matches === undefined) {
     return <HomeScreenSkeleton />;
   }
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => setRefreshing(false), 1000);
+  };
 
   return (
     <SafeAreaView className="flex-1 bg-background">
@@ -26,9 +33,19 @@ export default function HomeScreen() {
         renderItem={({ item }) => (
           <MatchCard match={item} />
         )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
         keyExtractor={(item) => item._id}
         contentContainerClassName="pb-16"
         showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          <View className="flex-1 items-center justify-center p-8">
+            <Text className="text-muted-foreground text-center">
+              No matches found.
+            </Text>
+          </View>
+        }
       />
     </SafeAreaView>
   );
